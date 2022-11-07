e56ef144f800d3bfd8b62027d68dea4d2937540f
andres-av <andres.av.dev@gmail.com>
2022-11-07 11:31:22 +0000

    initial commit with instagram model diagram

--- src/models.py
+++ src/models.py
@@ -8,23 +8,45 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+    __tablename__ = 'user'
+    id_user = Column(Integer, primary_key=True)
+    user_name = Column(String(50), nullable=False)
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    email = Column(String(50), nullable=False)
+    gender = Column(String(50), nullable=False)
+    like = relationship('Like', backref='user', lazy = True)
+    comment = relationship('Comment', backref='user', lazy = True)
+    follow = relationship('Follow', backref='user', lazy = True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Post (Base):
+    __tablename__ = 'post'
+    id_post = Column(Integer, primary_key=True)
+    photo_url = Column(String(100))
+    time_stamp = Column(String(50))
+    caption = Column(String(300), nullable=True)
+    like = relationship('Like', backref='post', lazy = True)
+
+class Comment (Base):
+    __tablename__ = 'comment'
+    id_comment = Column(Integer, primary_key=True)
+    comment_text = Column(String(200))
+    time_stamp = Column(String(50))
+    fk_user = Column(Integer, ForeignKey('user.id_user'), nullable = False)
+
+class Like(Base):
+    __tablename__ = 'like'
+    id_like = Column(Integer, primary_key=True)
+    fk_user = Column(Integer, ForeignKey('user.id_user'), nullable = False)
+    fk_post = Column(Integer, ForeignKey('post.id_post'))
+
+class Follow(Base):
+    __tablename__ = 'follow'
+    id_follow = Column(Integer, primary_key=True)
+    fk_user = Column(Integer, ForeignKey('user.id_user'), nullable = False)
+    fk_user = Column(Integer, ForeignKey('user.id_user'), nullable = False)
+
 
     def to_dict(self):
         return {}
